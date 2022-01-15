9e7d2a3ddb434e7f54cfebe59a8c91a74a6e654c
Akito Fujisaki <aki19035vc@gmail.com>
2022-01-15 17:15:20 +0900

    Fix

--- install.sh
+++ install.sh
@@ -1,16 +1,5 @@
 DOTROOT=$HOME/dotfiles
 
+ln -s $DOTROOT/Brewfile $HOME
+
 brew bundle --global
-
-install_list=(
-    "zsh"
-    "tmux"
-    "emacs"
-)
-
-for i in "${install_list[@]}"
-do
-    target=$DOTROOT/$i/install.sh
-    chmod u+x $target
-    $target
-done
